a03ca6d19b967624aa25ab38b9dd0939ee803561
angelherrador <angel@herradormartinez.es>
2023-11-30 16:25:58 +0100

    pasar coordenadas a map

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,8 +1,7 @@
-
 import 'package:activitat_1_4/models/zip_code_model.dart';
+import 'package:activitat_1_4/screens/map.dart';
 import 'package:activitat_1_4/services/zipcode_service.dart';
 import 'package:flutter/material.dart';
-import 'package:activitat_1_4/screens/map.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -13,10 +12,10 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   String city = "";
-  String ?longitude;
-  String ?latitude;
-  String ?state;
-  String ?postalCode;
+  String? longitude;
+  String? latitude;
+  String? state;
+  String? postalCode;
 
   //String zipCodeInput="23700";
 
@@ -25,6 +24,9 @@ class _HomeState extends State<Home> {
 
   void clearText() {
     zipCodeController.clear();
+    city = '';
+    longitude = '';
+    latitude = '';
   }
 
   @override
@@ -38,108 +40,117 @@ class _HomeState extends State<Home> {
     // }
     return Scaffold(
       appBar: AppBar(
-        title:const Text('Postal Code App'),
+        title: const Text('Postal Code App'),
       ),
-      body:Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-                SizedBox(
-                  width: screenWidth*0.8,
-                  child: const Text("Indica el codi postal per trobar la ciutat",
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.black45,)
-                  )
-                ),
-              SizedBox(
-                width:250,
-                child: TextField(
-                  autofocus: true,
-                  onChanged: (zipInput) async {
-                      ZipCode? zipCode = await ZipCodeService().fetchData(zipInput);
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          SizedBox(
+              width: screenWidth * 0.8,
+              child: const Text("Indica el codi postal per trobar la ciutat",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black45,
+                  ))),
+          SizedBox(
+            width: 250,
+            child: TextField(
+              autofocus: true,
+              onChanged: (zipInput) async {
+                ZipCode? zipCode = await ZipCodeService().fetchData(zipInput);
 
-                      city = zipCode.places.first.placeName;
-                      longitude = zipCode.places.first.longitude;
-                      latitude = zipCode.places.first.latitude;
-                      state = zipCode.places.first.state;
-                      postalCode= zipCode.postCode;
+                city = zipCode.places.first.placeName;
+                longitude = zipCode.places.first.longitude;
+                latitude = zipCode.places.first.latitude;
+                state = zipCode.places.first.state;
+                postalCode = zipCode.postCode;
 
-                      debugPrint(city);
-                      debugPrint(longitude);
-                      debugPrint(state);
-                      setState(() {});
-                    },
-                  controller: zipCodeController,
-                  decoration: InputDecoration(
-                    //prefixIcon: const Icon(Icons.search),
-                    suffixIcon: IconButton(
-                          icon: const Icon(Icons.clear,),
-                          onPressed: clearText, //zipCodeController.clear,
-                    ),
-                    border: const OutlineInputBorder(),
-
+                debugPrint(city);
+                debugPrint(longitude);
+                debugPrint(state);
+                setState(() {});
+              },
+              controller: zipCodeController,
+              decoration: InputDecoration(
+                //prefixIcon: const Icon(Icons.search),
+                suffixIcon: IconButton(
+                  icon: const Icon(
+                    Icons.clear,
                   ),
-                  style: const TextStyle(fontSize: 24),
-                  textAlign: TextAlign.center,
+                  onPressed: clearText, //zipCodeController.clear,
                 ),
+                border: const OutlineInputBorder(),
               ),
-              if (postalCode==null)
-                 Text("Codi Postal ${zipCodeController.text} no trobat o no existeix!!"),
-              if (postalCode != null)
-                Text('Codi Postal: $postalCode'),
-              if (longitude != null)
-                Text('Ciutat: $city', style: const TextStyle(fontSize: 24),),
-              if (longitude != null)
-                Text('Comunitat: $state'),
-              if (longitude != null)
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      width: screenWidth*0.1,
-                      child: const Icon(Icons.place),),
-                    SizedBox(
-                        width: screenWidth*0.25,
-                        child: const Text('Coordenades:'),),
-                    SizedBox(
-                      width: screenWidth*0.25,
-                        child: Text('Long: $longitude'),),
-                    SizedBox(
-                      width: screenWidth*0.3,
-                        child:Text('Lat: $latitude'),),
-                  ],
+              style: const TextStyle(fontSize: 24),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          if (postalCode == null)
+            Text(
+                "Codi Postal ${zipCodeController.text} no trobat o no existeix!!"),
+          if (postalCode != null) Text('Codi Postal: $postalCode'),
+          if (longitude != null)
+            Text(
+              'Ciutat: $city',
+              style: const TextStyle(fontSize: 24),
+            ),
+          if (longitude != null) Text('Comunitat: $state'),
+          if (longitude != null)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: screenWidth * 0.1,
+                  child: const Icon(Icons.place),
                 ),
-              ElevatedButton(
-                  onPressed: (){
-                    //openGoogleMaps();
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => MapSituation(city)));
-                  },
-                  child: const Text("Mostrar en Google Maps")
-              ),
-          ]),
-        ),
-        // floatingActionButton: FloatingActionButton(
-        //   onPressed: ()
-        //     async {
-        //       var zipcode = await ZipCodeService().fetchData("43001");
-        //
-        //       city = zipcode.places.first.placeName;
-        //       longitude = zipcode.places.first.longitude;
-        //       latitude = zipcode.places.first.latitude;
-        //       state = zipcode.places.first.state;
-        //
-        //       debugPrint(city);
-        //       debugPrint(longitude);
-        //       debugPrint(state);
-        //       setState(() {});
-        //     },
-        //
-        //     child: const Icon(Icons.refresh),
-        // ),
+                SizedBox(
+                  width: screenWidth * 0.25,
+                  child: const Text('Coordenades:'),
+                ),
+                SizedBox(
+                  width: screenWidth * 0.25,
+                  child: Text('Lat: $latitude'),
+                ),
+                SizedBox(
+                  width: screenWidth * 0.3,
+                  child: Text('Long: $longitude'),
+                ),
+              ],
+            ),
+          ElevatedButton(
+              onPressed: () {
+                //openGoogleMaps();
+                city = 'Linares';
+                latitude = '20';
+                longitude = '50';
+                if (city == 'Linares') {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              MapSituation(city, latitude!, longitude!)));
+                }
+              },
+              child: const Text("Mostrar en Google Maps")),
+        ]),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: ()
+      //     async {
+      //       var zipcode = await ZipCodeService().fetchData("43001");
+      //
+      //       city = zipcode.places.first.placeName;
+      //       longitude = zipcode.places.first.longitude;
+      //       latitude = zipcode.places.first.latitude;
+      //       state = zipcode.places.first.state;
+      //
+      //       debugPrint(city);
+      //       debugPrint(longitude);
+      //       debugPrint(state);
+      //       setState(() {});
+      //     },
+      //
+      //     child: const Icon(Icons.refresh),
+      // ),
     );
   }
 }
-
--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -1,28 +1,32 @@
 import 'package:flutter/material.dart';
 
 class MapSituation extends StatelessWidget {
-   MapSituation(this.city, {super.key});
+  const MapSituation(this.city, this.latitude, this.longitude, {super.key});
 
-  String city;
+  final String city;
+  final String latitude;
+  final String longitude;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title:const Text('Situation Map'),
-        ),
-      body:
-        Column(
-          children: [
-            Text('Coordenadas: $city', style: TextStyle(fontSize: 20),)
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: (){
-            Navigator.pop(context);
-          },
-
-          child: const Icon(Icons.exit_to_app),
-        ),
-      );
+        title: const Text('Situation Map'),
+      ),
+      body: Column(
+        children: [
+          Text(
+            'Coordenadas: $city - Lat: $latitude, Lon: $longitude',
+            style: const TextStyle(fontSize: 20),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: const Icon(Icons.exit_to_app),
+      ),
+    );
   }
 }
